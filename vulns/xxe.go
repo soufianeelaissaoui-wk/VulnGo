a555b6a5a28ebf5be90b0b0e7ec98a2bc898467b
Soufiane <soufiane.elaissaoui@workiva.com>
2023-11-30 16:48:00 +0100

    add xxe

--- vulns/xxe.go
+++ vulns/xxe.go
@@ -1,25 +1,11 @@
 package vulns
 
 import (
-	"encoding/xml"
 	"fmt"
+	"github.com/lestrrat-go/libxml2/parser"
 	"net/http"
 )
 
-type User struct {
-	XMLName xml.Name `xml:"user"`
-	ID      int      `xml:"id"`
-	Name    string   `xml:"name"`
-}
-
-func parseXML(data []byte) (*User, error) {
-	var user User
-	err := xml.Unmarshal(data, &user)
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
-}
 
 func Xxe(w http.ResponseWriter, r *http.Request) {
 	xmlData := r.URL.Query().Get("xmlData")
@@ -29,11 +15,14 @@ func Xxe(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	user, err := parseXML([]byte(xmlData))
+	p := parser.New(parser.XMLParseNoEnt)
+	doc, err := p.ParseString(xmlData)
+	defer doc.Free()
+
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error parsing XML: %v", err), http.StatusInternalServerError)
 		return
 	}
 
-	fmt.Fprintf(w, "Parsed user: %+v\n", user)
+	fmt.Fprintf(w, "Parsed user: %+v\n", doc)
 }
